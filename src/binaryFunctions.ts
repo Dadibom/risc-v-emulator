dbc2b98b9a0f6fc59d17a2e37904ba76e85ec80b
Alice Parrish <alice@pop-os.localdomain>
2023-07-13 18:23:35 -0400

    Added edge cases to binary function tests

--- src/binaryFunctions.ts
+++ src/binaryFunctions.ts
@@ -30,8 +30,21 @@ export const getBitTestCases: Map<number[], number> = new Map([
   [[32, 4], 0],
   [[32, 5], 1],
   [[32, 6], 0],
+  [[24, 2], 0],
+  [[24, 3], 1],
+  [[24, 4], 1],
+  [[24, 5], 0],
+  [[48, 3], 0],
+  [[48, 4], 1],
+  [[48, 5], 1],
+  [[0x80000000, 30], 0],
+  [[0x80000000, 31], 1]
 ]);
 
 export const getRangeTestCases: Map<number[], number> = new Map([
-  [[0xFF, 3, 0], 0xF]
+  [[0xFF, 3, 0], 0xF],
+  [[0xFFFFFFFF, 31, 0], 0xFFFFFFFF],
+  [[0xFFFFFFFF, 30, 0], 0x7FFFFFFF],
+  [[0xFFFFFFFF, 12, 9], 0xF],
+  [[0x0A0B0C0D, 23, 8], 0x0B0C]
 ]);
