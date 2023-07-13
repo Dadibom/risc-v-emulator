901b640db39743fb8b72361c4444dff8cb68d414
Alice Parrish <alice@pop-os.localdomain>
2023-07-13 17:23:19 -0400

    Added binary helper functions

---
+++ src/binaryFunctions.ts
@@ -0,0 +1,27 @@
+// Returns bits from 32-bit input in specified range
+export function getRange(input: number, upperEnd: number, lowerEnd: number): number | null {
+  if (upperEnd > 31 ||
+      lowerEnd < 0 ||
+      lowerEnd > upperEnd ||
+      lowerEnd === upperEnd
+    ) return null;
+
+  let result = input;
+
+  if (upperEnd < 31) {
+    const bitmask: number = 0xFFFFFFFF << upperEnd;
+    result &= bitmask;
+  }
+
+  return result >>> lowerEnd;
+
+}
+
+// Returns specified bit from 32-bit input
+export function getBit(input: number, index: number): number | null {
+  if (index > 31 ||
+      index < 0
+    ) return null
+
+  return (input >>> index) & 0x00000001
+}
