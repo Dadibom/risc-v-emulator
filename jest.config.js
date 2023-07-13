b9f2cf96d5fc5f723c464e7976fd9bc0636e5f1b
Alice Parrish <alice@pop-os.localdomain>
2023-07-13 18:06:18 -0400

    Added citation for jest config

--- jest.config.js
+++ jest.config.js
@@ -4,3 +4,5 @@ module.exports = {
   testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
+
+// Configured following this guide: https://www.testim.io/blog/typescript-unit-testing-101/
