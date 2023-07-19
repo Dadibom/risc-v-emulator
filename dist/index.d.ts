a601e27c62ec0706c567f53574498474b48ddd12
Alice Parrish <alice@pop-os.localdomain>
2023-07-19 14:40:41 -0400

    Regenerated package-lock.json

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,8 @@
 import { Assembler } from "./Assembler/assembler";
-import { CPU } from "./cpu";
+import { CPU, RegisterSet } from "./cpu";
 declare const _default: {
     CPU: typeof CPU;
+    RegisterSet: typeof RegisterSet;
     Assembler: typeof Assembler;
 };
 export default _default;
--- dist/index.js
+++ dist/index.js
@@ -4,5 +4,6 @@ const assembler_1 = require("./Assembler/assembler");
 const cpu_1 = require("./cpu");
 exports.default = {
     CPU: cpu_1.CPU,
+    RegisterSet: cpu_1.RegisterSet,
     Assembler: assembler_1.Assembler
 };
